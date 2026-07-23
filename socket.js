ed898691788e15a7bda880f029f1f8dd3900c9b4
dheerajinfograins <dheerajgami@infograins.com>
2026-07-23 16:57:41 +0530

    chore: update CORS configuration to allow all origins in socket server

--- socket.js
+++ socket.js
@@ -6,7 +6,9 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "http://127.0.0.1:5173"], 
+      origin: function (origin, callback) {
+        callback(null, true);
+      },
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     },
